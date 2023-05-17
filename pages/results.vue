305acb2ffc0e7c553607dff2e47a5957e4b8edd9
Yuta Takahashi <m1205123ing@gmail.com>
2023-05-17 21:34:06 +0900

    LINE link

--- pages/results.vue
+++ pages/results.vue
@@ -1,11 +1,33 @@
 <template>
   <v-container>
-    <v-card>
-      <v-card-title>結果</v-card-title>
-      <v-card-text>
-        <radar-chart :chartData="chartData"></radar-chart>
-      </v-card-text>
-    </v-card>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>結果</v-card-title>
+          <v-card-text>
+            <radar-chart :chartData="chartData"></radar-chart>
+        </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-text>
+            結果はいかがでしたか？<br>
+            今後の恋愛に役立てていただければ幸いです😊<br>
+            <br>
+            また、こちらのモテチェッカーは3カテゴリーのみのチェックとなっております。<br>
+            より詳しく（6カテゴリーで）チェックしたい方、<br>
+            身近にチェックしてくれる人がいない方は、<br>
+            ぜひ下記のLINE公式アカウントをご利用ください♫😊<br>
+        </v-card-text>
+          <v-card-actions class="justify-center">
+            <a :href="officialLineURL"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="56" border="0"></a>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -18,6 +40,11 @@ import { Category, Question } from '../types/survey'
 export default defineComponent({
   components: {
     RadarChart,
+  },
+  data() {
+    return {
+      officialLineURL: 'https://bit.ly/3LrZHea'
+    }
   },
   computed: {
     ...mapState({
